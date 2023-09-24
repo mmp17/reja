34fa114b12e5f2948a10241ac85bed15304b534f
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-09-25 06:51:26 +0900

    Jack Ma maslahatlari: Callback function

--- train.js
+++ train.js
@@ -1,23 +1,66 @@
 // =================================================================
-// D-Task:
+// Jack Ma maslahatlari: Callback function
 // =================================================================
 
-const Shop = require("./section_three/shop");
-const shop = new Shop(10, 10, 10);
-// const shop2 = new Shop(1, 2, 1);
+console.log("Jack Ma maslahatlari");
 
-remainder = shop.remainder();
-salesResult = shop.sales("bread", 1);
-procurementResult = shop.procurement("bread", 1);
-console.log(remainder);
-// console.log(salesResult);
-// console.log(procurementResult);
-// console.log(remainder);
+const list = [
+  "Yaxshi talaba bo'ling", // 0-20
+  "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+  "O'zingizga ishlashni boshlang", // 30-40
+  "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+  "Yoshlarga investitsiya qiling", // 50-60
+  "Endi dam oling, foydasi yo'q endi", // 60
+];
 
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("Enter a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    // callback(null, list[5]);
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+console.log("Passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.error("Error:", err);
+  else {
+    console.log("Javob:", data);
+  }
+});
+console.log("Passed here 1");
+
+// OutputL:
+// Jack Ma maslahatlari
+// Passed here 0
+// Passed here 1
+// Javob: Endi dam oling, foydasi yo'q endi
+
+// =================================================================
+// D-Task:
+// =================================================================
 // Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin,
 //biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
 // MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non,
 //5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// const Shop = require("./section_three/shop");
+// const shop = new Shop(10, 10, 10);
+
+// remainder = shop.remainder();
+// salesResult = shop.sales("bread", 1);
+// procurementResult = shop.procurement("bread", 1);
+// console.log(remainder);
+// console.log(salesResult);
+// console.log(procurementResult);
+// console.log(remainder);
 
 // =================================================================
 // C-Task:
