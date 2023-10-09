70032a0b55db2d227afe79e3d465f8bb0ca99f60
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-10-09 23:54:07 +0900

    fix: modify port 7007

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongodb.connect(
       const app = require("./app");
 
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 7007;
       server.listen(PORT, function () {
         console.log(
           `server is running on port ${PORT}, http://localhost:${PORT}`
